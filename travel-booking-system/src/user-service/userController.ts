35120676f367efaefd345a78765c15612bea6a60
ahzia <ah.ziayosfi@gmail.com>
2024-10-01 15:07:39 +0430

    GET and POST /user using ORM

--- travel-booking-system/src/user-service/userController.ts
+++ travel-booking-system/src/user-service/userController.ts
@@ -1,48 +1,51 @@
 import { Request,Response } from "express";
-import User from "../modals/userModel.js";
+import { User } from "../entities/User.js";
+import { AppDataSource } from "../database/ormconfig.js";
+
+const userRepo = AppDataSource.getRepository(User);
 
 const get = async (req: Request,res: Response) => {
-    const users = await User.find();
-    res.send(users);
+    try{
+        const users = await userRepo.find();
+        res.send(users);
+    } catch(error) {
+        res.status(500).send(error)
+    }
 };
 
 const getByID =  async (req: Request, res: Response) => {
     const userId = req.params.id;
-    const user = await User.findById(userId);
-    if (!user) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    res.status(200).send(user); // Respond with the found user
+    // const user = await User.findById(userId);
+    // if (!user) {
+    //     return res.status(404).send({ message: 'User not found' });
+    // }
+    // res.status(200).send(user); // Respond with the found user
 }
 
 const create = async (req: Request,res: Response) => {
-
     const {name, email} = req.body;
-    const user  = new User({
-        name,
-        email
-    });
-    await user.save()
+    const user = userRepo.create({name, email});
+    await userRepo.save(user);
     res.status(201).send(user);
 }
 
 const update =  async (req: Request,res: Response) => {
     const userId = req.params.id;
     const { name, email } = req.body;
-    const user = await User.findByIdAndUpdate(userId, {name, email}, {new: true});
-    if (user) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    res.status(200).send(user); // Respond with the updated user
+    // const user = await User.findByIdAndUpdate(userId, {name, email}, {new: true});
+    // if (user) {
+    //     return res.status(404).send({ message: 'User not found' });
+    // }
+    // res.status(200).send(user); // Respond with the updated user
 }
 
 const remove =  async (req: Request,res: Response) => {
     const userId = req.params.id;
-    const user = await User.findByIdAndDelete(userId)
-    if (user) {
-        return res.status(404).send({ message: 'User not found' });
-    }
-    res.status(204).send(); // Respond with no content
+    // const user = await User.findByIdAndDelete(userId)
+    // if (user) {
+    //     return res.status(404).send({ message: 'User not found' });
+    // }
+    // res.status(204).send(); // Respond with no content
 }
 
 export default {get, getByID, create, update, remove};
